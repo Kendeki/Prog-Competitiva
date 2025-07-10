9b13c43b0294ab77dfbec8a48f915293ead2a491
Rodrigo Monteiro Junior <71048858+TsukiGva2@users.noreply.github.com>
2025-07-09 21:27:21 -0300

    Probleminha fácil só pra dar uma relaxada

---
+++ Problemas/Codeforces/281/A/a.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/281/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char c;
+  cin >> c;
+  c = toupper(c);
+  string s;
+  cin >> s;
+  cout << c;
+  cout << s;
+}
