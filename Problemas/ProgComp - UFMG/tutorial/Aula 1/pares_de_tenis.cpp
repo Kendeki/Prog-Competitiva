e56868123e6e2ab225cfdd612220410af4620b18
Rodrigo Monteiro Junior <71048858+TsukiGva2@users.noreply.github.com>
2025-07-15 13:12:53 -0300

    problema do tutorial só que burro

---
+++ Problemas/ProgComp - UFMG/tutorial/Aula 1/pares_de_tenis.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+
+void solve()
+{
+  char c;
+  ll n, sz, p;
+  cin >> n;
+  
+  vector<pair<ll, ll>> tamanho(60, pair<ll,ll>(0, 0));
+  
+  p = 0;
+  
+  while (n--)
+  {
+    cin >> sz >> c;
+    
+    ll D, E;
+    D = tamanho[sz].first;
+    E = tamanho[sz].second;
+    
+    tamanho[sz] = make_pair(
+      c == 'D' ? ++D : D,
+      c == 'E' ? ++E : E
+    );
+    
+    if (D and E)
+    {
+      p++;
+      tamanho[sz] = make_pair(--D, --E);
+    }
+  }
+  
+  cout << p << endl;
+}
+
+int main() 
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  #define SINGLE true
+
+  if (!SINGLE)
+  {
+    ll t; cin >> t;
+    while(t--)solve();
+  } else solve();
+  
+  return 0;
+}
