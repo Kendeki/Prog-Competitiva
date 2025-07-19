aecaf9099ce498294814cd6a7a955276f721293f
Rodrigo Monteiro Junior <71048858+TsukiGva2@users.noreply.github.com>
2025-07-18 21:54:17 -0300

    Create macros.cpp

---
+++ Util/macros.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define VAL(x) cout << #x << ": " << x << '\n';
+#define IN(dest) cin >> dest;
+#define ANS(x) cout << x << '\n';
+#define FOR(I, n) for(ll I=0;I<n;I++)
