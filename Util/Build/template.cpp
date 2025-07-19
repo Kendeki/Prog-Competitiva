29f0144eab1485154f76381d8906d9fe75fc89f4
Rodrigo Monteiro Junior <71048858+TsukiGva2@users.noreply.github.com>
2025-07-18 21:57:48 -0300

    Update template.cpp

--- Util/Build/template.cpp
+++ Util/Build/template.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 typedef long long ll;
+typedef vector<ll> vll;
+#define endl '\n'
+
+#define VAL(x) cout << #x << ": " << x << '\n';
+#define IN(dest) cin >> dest;
+#define ANS(x) cout << x << '\n';
+#define FOR(I, n) for(ll I=0;I<n;I++)
 
 void solve()
 {
@@ -10,19 +15,15 @@ void solve()
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  
 #define SINGLE true
 
-	if (!SINGLE)
-	{
-		ll t; cin >> t;
-		while (t--) solve();
-	}
-	else solve();
-
-	return 0;
+  if (!SINGLE)
+  {
+    ll t; cin >> t;
+    while(t--)solve();
+  } else solve();
 }
-
