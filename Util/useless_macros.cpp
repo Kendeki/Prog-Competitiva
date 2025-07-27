90161505f451f8d75f01b9e0aa32450eca84893a
TsukiGva2 <tsukigva@gmail.com>
2025-07-27 10:51:22 -0300

    :)

--- Util/useless_macros.cpp
+++ Util/useless_macros.cpp
@@ -1,11 +1,11 @@
 
-// https://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
-#define DIVCEIL(x, y) x == 0 ? 0 : 1 + ((x - 1) / y);
-
 // SQL's IF, i just found it really funny
 template <typename T>
 inline T IF(bool condition, T val, T elseval)
 {
 	return condition ? val : elseval;
 }
+
+// https://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
+#define DIVCEIL(x, y) IF(x == 0, 0, 1 + ((x - 1) / y)
 
