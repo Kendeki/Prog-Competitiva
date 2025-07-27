0520c7160060d6a1275aee612e5f23beded2342f
TsukiGva2 <tsukigva@gmail.com>
2025-07-27 10:54:35 -0300

    Consertando erro besta (esqueci de fechar parens )

--- Util/useless_macros.cpp
+++ Util/useless_macros.cpp
@@ -7,5 +7,5 @@ inline T IF(bool condition, T val, T elseval)
 }
 
 // https://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
-#define DIVCEIL(x, y) IF(x == 0, 0, 1 + ((x - 1) / y)
+#define DIVCEIL(x, y) IF(x == 0, 0, 1 + ((x - 1) / y))
 
