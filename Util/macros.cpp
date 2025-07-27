ab1aac23938239dfee094037be84888217561a1c
TsukiGva2 <tsukigva@gmail.com>
2025-07-27 10:48:26 -0300

    commit meme

--- Util/macros.cpp
+++ Util/macros.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 typedef long long ll;
 
-// https://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
-#define DIVCEIL(x, y) x == 0 ? 0 : 1 + ((x - 1) / y);
 #define VAL(x) cout << #x << ": " << x << '\n';
 #define IN(dest) cin >> dest;
 #define ANS(x) cout << x << '\n';
 #define FOR(I, n) for(ll I=0;I<n;I++)
+
