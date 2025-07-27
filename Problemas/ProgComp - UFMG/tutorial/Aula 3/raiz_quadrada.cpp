d5e9b995b8a6eb3f8db65f451a8157534ae3d339
TsukiGva2 <tsukigva@gmail.com>
2025-07-27 17:37:57 -0300

    Resolvido novo problema de busca binária com double

--- Problemas/ProgComp - UFMG/tutorial/Aula 3/raiz_quadrada.cpp
+++ Problemas/ProgComp - UFMG/tutorial/Aula 3/raiz_quadrada.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef vector<ll> vll;
-#define endl '\n'
 
-#define VAL(x) cout << #x << ": " << x << '\n';
 #define IN(dest) cin >> dest;
 #define ANS(x) cout << x << '\n';
 #define FOR(I, n) for(ll I=0;I<n;I++)
 
 void solve()
 {
+	cout << fixed << setprecision(10);
+	double N; IN(N);
+	double l = 0, r = 1e8+10; // limite: 1 < N < 10^16, logo, raiz max = 10^8 ou 1e8
+	FOR(i, 200) // numero fixo de iterações
+	{
+		double m = (l+r)/2;
+		if (m*m > N) r = m;
+		else l = m;
+	}
+	ANS((l+r)/2);
 }
 
 int main()
@@ -18,7 +25,7 @@ int main()
   ios_base::sync_with_stdio(0);
   cin.tie(nullptr);
   cout.tie(nullptr);
-
+  
 #define SINGLE
 
 #ifndef SINGLE
